fb58a706a0d35e6abcecea5cfc95c07f1ab9806c
arindamrun <anirban9879@gmail.com>
2024-06-16 21:31:22 +0530

    dgtalist file upload

--- src/app/api/casestudies/route.js
+++ src/app/api/casestudies/route.js
@@ -1,8 +1,9 @@
-import mongoose from 'mongoose';
-import { Casestudy } from './model/casestudy';
-import { NextResponse } from 'next/server';
+import mongoose from "mongoose";
+import { Casestudy } from "./model/casestudy";
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import path from "path";
 
-// Connect to MongoDB
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
 
@@ -12,26 +13,48 @@ const connectDB = async () => {
   });
 };
 
-// Handler for POST request
 export async function POST(request) {
   await connectDB();
-  const body = await request.json();
-  
-  console.log(body);
 
+  // Parse form data
+  const formData = await request.formData();
+  const file = formData.get("logo");
+
+  let logoPath = "";
+  // Save file to public/uploads directory
+  if (file) {
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const filename = `${Math.random()}-${file.name}`;
+    const filePath = path.join(process.cwd(), "public", "images", filename);
+    fs.access("./public/images", fs.constants.F_OK)
+    .then(() => {
+      console.log('Directory exists.');
+    })
+    .catch(() => {
+      fs.mkdir('images', {recursive:true});
+      console.log('Directory does not exist.');
+    });
+    await fs.writeFile(filePath, buffer);
+    logoPath = `/${filename}`;
+  }
   const casestudy = new Casestudy({
-    logo: body.logo,
-    casetitle: body.casetitle,
-    casecontent: body.casecontent,
-    tstacktitle: body.tstacktitle,
-    rightbanner: body.rightbanner
+    logo: logoPath,
+    casetitle:formData.get("casetitle"),
+    casecontent:formData.get("casecontent"),
+    tstacktitle: formData.get("tstacktitle"),
+    rightbanner:formData.get("rightbanner"),
   });
 
-  // Save the case study to the database
   try {
     await casestudy.save();
-    return NextResponse.json({ message: 'Case study saved successfully!', casestudy });
+    return NextResponse.json({
+      message: "Case study saved successfully!",
+      casestudy,
+    });
   } catch (error) {
-    return NextResponse.json({ message: 'Error saving case study', error }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error saving case study", error },
+      { status: 500}
+    );
   }
 }
